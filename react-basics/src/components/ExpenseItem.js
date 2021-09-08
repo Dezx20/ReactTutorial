b5a461a2e48ae056a6646b2cdd13f0cca03df962
Dezx20 <2009awektoppo@gmail.com>
2021-09-09 02:04:27 +0530

    assignment 1 done

--- react-basics/src/components/ExpenseItem.js
+++ react-basics/src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseDate = props.date;
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
-
   return (
     <div className="expense-item">
-      <div> {expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2> {expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount }</div>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
