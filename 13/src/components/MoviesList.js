bb552972c00b8e109dd351682e0b53b473948d08
Dezx20 <2009awektoppo@gmail.com>
2021-09-16 16:39:53 +0530

    custom hooks

--- 13/src/components/MoviesList.js
+++ 13/src/components/MoviesList.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
 
 const MovieList = (props) => {
+  // console.log(props.movies);
   return (
-    <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
-        />
-      ))}
+    <ul className={classes["movies-list"]}>
+      {props.movies.map((movie) => {
+        return (
+          <Movie
+            key={movie.id}
+            title={movie.title}
+            releaseDate={movie.releaseDate}
+            openingText={movie.openingText}
+          />
+        );
+      })}
     </ul>
   );
 };
